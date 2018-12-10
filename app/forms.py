974133eebc38308dce2c9ef5a04a9e0a31f158c3
Sagar Vrajalal <sagar7666@gmail.com>
2018-12-10 22:46:44 +0530

    added registration form validators

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, EqualTo
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -10,5 +10,9 @@ class LoginForm(FlaskForm):
 class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    repeatedPassword = PasswordField('Confirm Password', validators=[DataRequired()])
+    repeatedPassword = PasswordField('Confirm Password', validators=[
+                                        DataRequired(),
+                                        EqualTo('password', message='Please enter the correct password!')
+                                                                    ]
+                                    )
     submit = SubmitField('Register')
