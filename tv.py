be34f8fcc77269255831774a847510beea5d15f4
Sagar Vrajalal <sagar7666@gmail.com>
2018-12-11 23:20:12 +0530

    deleted __init__.py file and moved main app logic to tv.py

--- tv.py
+++ tv.py
@@ -1,1 +1,32 @@
-from app import app
+# < ---------------------------------- > #
+
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from app.routes import app_routing
+
+# < ---------------------------------- > #
+
+app = Flask(__name__)
+
+# < ---------------------------------- > #
+
+app.register_blueprint(app_routing)
+
+# < ---------------------------------- > #
+
+app.config.from_object(Config)
+
+# < ---------------------------------- > #
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# < ---------------------------------- > #
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# < ---------------------------------- > #
