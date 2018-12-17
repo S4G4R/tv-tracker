15226c593c95d668e3c1abf411e8ff0711ee9d60
Sagar Vrajalal <sagar7666@gmail.com>
2018-12-17 17:33:34 +0530

    cleaned up

--- tv.py
+++ tv.py
@@ -5,7 +5,6 @@ from config import Config
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from app.routes import app_routing
 
 # < ---------------------------------- > #
 
@@ -13,16 +12,7 @@ app = Flask(__name__)
 
 # < ---------------------------------- > #
 
-app.register_blueprint(app_routing)
-
-# < ---------------------------------- > #
-
 app.config.from_object(Config)
-
-# < ---------------------------------- > #
-
-login_manager = LoginManager()
-login_manager.init_app(app)
 
 # < ---------------------------------- > #
 
@@ -31,5 +21,17 @@ migrate = Migrate(app, db)
 
 from app import models
 
+db.create_all()
+
 # TODO Set up database migration
+# < ---------------------------------- > #
+
+from app.routes import app_routing
+app.register_blueprint(app_routing)
+
+# < ---------------------------------- > #
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
 # < ---------------------------------- > #
