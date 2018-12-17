65594ad9b624c0e2f03372a4e5d5a3847e0178a9
Sagar Vrajalal <sagar7666@gmail.com>
2018-12-17 21:24:02 +0530

    added login authentication features

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
-from flask import render_template, request, redirect, Blueprint, flash
+from flask import render_template, redirect, Blueprint, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from app.forms import LoginForm, RegisterForm
 from app.models import User
-from tv import db
+from flask_login import login_user, logout_user, login_required, current_user
+from tv import db, login_manager
 
 app_routing = Blueprint('app_routing',__name__)
 
@@ -11,6 +12,10 @@ app_routing = Blueprint('app_routing',__name__)
 def index():
     return render_template('index.html')
 
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(id)
+
 @app_routing.route('/login', methods=['GET','POST'])
 def login():
 
@@ -18,12 +23,48 @@ def login():
 
     if form.validate_on_submit():
 
-        # TODO LOGIN LOGIC
+        username = form.username.data
+
+        username_present = User.query.filter_by(username=username).first()
+
+        if not username_present:
+            flash('Incorrect password or username!')
+            return redirect('/index')
+
+        current_user = username_present
+
+        password = form.password.data
+        pw_hash = username_present.pw_hash
+
+        if not check_password_hash(pw_hash, password):
+            flash('Incorrect password or username!')
+            return redirect('/index')
+
+        current_user.authenticated = True
+        db.session.add(current_user)
+        db.session.commit()
+
+        login_user(current_user, remember=True)
 
         flash('Logged in successfully!')
         return redirect('/index')
 
     return render_template('login.html', form=form)
+
+@app_routing.route('/logout', methods=['GET'])
+@login_required
+def logout():
+
+    user = current_user
+    user.authenticated = False
+    db.session.add(user)
+    db.session.commit()
+
+    logout_user()
+
+    flash('Logged out successfully!')
+
+    return redirect('/index')
 
 @app_routing.route('/register', methods=['GET','POST'])
 def register():
