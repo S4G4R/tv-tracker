da0396af99a635255b86196394783a6091ba9eea
Sagar Vrajalal <sagar7666@gmail.com>
2018-12-17 21:23:31 +0530

    added authentication details

--- app/models.py
+++ app/models.py
@@ -4,6 +4,19 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False)
     pw_hash = db.Column(db.String(80), nullable=False)
+    authenticated = db.Column(db.Boolean, default=False)
+
+    def is_authenticated(self):
+        return self.authenticated
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return self.authenticated
+
+    def get_id(self):
+        return self.id
 
     def __repr__(self):
         return '<User %r>' % self.username
