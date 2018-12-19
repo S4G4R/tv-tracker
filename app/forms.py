aaacf340647dc896ba5112aa74a466056ed478d7
Sagar Vrajalal <sagar7666@gmail.com>
2018-12-19 23:19:49 +0530

    added search form

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
+from wtforms.validators import DataRequired, EqualTo, NumberRange
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -11,8 +11,14 @@ class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     repeatedPassword = PasswordField('Confirm Password', validators=[
-                                        DataRequired(),
-                                        EqualTo('password', message='Please enter the correct password!')
-                                                                    ]
-                                    )
+                    DataRequired(),
+                    EqualTo('password', message='Please enter the correct password!')])
     submit = SubmitField('Register')
+
+class SearchForm(FlaskForm):
+    type = SelectField('Choose one', choices=[
+        ('tv', 'TV Show'),
+        ('movie', 'Movie')
+    ])
+    title = StringField('Title Of TV Show/Movie', validators=[DataRequired()])
+    submit = SubmitField('Search')
