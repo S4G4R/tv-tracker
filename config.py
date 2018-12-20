feba0256c0a3e7282518d2cc5f3d75e43a3fb8fa
Sagar Vrajalal <sagar7666@gmail.com>
2018-12-20 22:15:50 +0530

    updated

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 import os
+import tmdb
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'databases/shows.db')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-secure-key-here'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////' + os.path.join(basedir, 'databases/shows.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    tmdb.API_KEY = 'your-api-key-here'
