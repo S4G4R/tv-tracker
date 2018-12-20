24d20ab3802b34816e5bce734bc5c7cf9f4e3a96
Sagar Vrajalal <sagar7666@gmail.com>
2018-12-20 22:27:05 +0530

    fixed typo 'tmdb' to 'tmdbsimple'

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
-import tmdb
+import tmdbsimple
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-secure-key-here'
     SQLALCHEMY_DATABASE_URI = 'sqlite:////' + os.path.join(basedir, 'databases/shows.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    tmdb.API_KEY = 'your-api-key-here'
+    tmdbsimple.API_KEY = 'your-api-key-here'
