ce1553ab397f13319808824f2f3d52dd1ddfa1da
Sagar Vrajalal <sagar7666@gmail.com>
2018-12-22 23:20:17 +0530

    created tables for movies and shows

--- app/models.py
+++ app/models.py
@@ -5,6 +5,8 @@ class User(db.Model):
     username = db.Column(db.String(80), nullable=False)
     pw_hash = db.Column(db.String(80), nullable=False)
     authenticated = db.Column(db.Boolean, default=False)
+    movies = db.relationship('Movie', backref='user')
+    shows = db.relationship('Show', backref='user')
 
     def is_authenticated(self):
         return self.authenticated
@@ -20,3 +22,15 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+class Movie(db.Model):
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    overview = db.Column(db.String(1000))
+
+class Show(db.Model):
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    overview = db.Column(db.String(1000))
