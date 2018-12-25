a5576ea7134f46290e5a67a7fe1c4390f66c86c4
Sagar Vrajalal <sagar7666@gmail.com>
2018-12-25 20:49:30 +0530

    rename, cleanup and add rating

--- app/models.py
+++ app/models.py
@@ -24,9 +24,10 @@ class User(db.Model):
         return '<User %r>' % self.username
 
 class Movie(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    movie_id = db.Column(db.Integer, primary_key=True)
     movie_name = db.Column(db.String(80), nullable=False)
-    status = db.Column(db.String(20), nullable=False)
+    status = db.Column(db.String(20), nullable=False, default='Not Watched')
+    rating = db.Column(db.Integer, default=0)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class Show(db.Model):
