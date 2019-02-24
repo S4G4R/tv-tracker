e0b037baf714bc3ba9b30a36f3bcb409f1c47024
Sagar Vrajalal <sagar7666@gmail.com>
2019-02-25 00:19:35 +0530

    clean up

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 import os
 import tmdbsimple
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+dotenv_path = os.path.join(basedir, '.env')
+load_dotenv(dotenv_path)
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-secure-key-here'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:////' + os.path.join(basedir, 'databases/shows.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    tmdbsimple.API_KEY = 'your-api-key-here'
+    SQLALCHEMY_DATABASE_URI         = 'sqlite:////' + os.path.join(basedir, 'databases/shows.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS  = False
+    SECRET_KEY                      = os.environ.get("SECRET_KEY")
+    tmdbsimple.API_KEY              = os.environ.get("API_KEY")
